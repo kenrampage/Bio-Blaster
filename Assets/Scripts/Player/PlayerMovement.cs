34bbe06e2cf523ee05d48e112bbf22bf3caf831d
Ken Rampage <73263295+kenrampage@users.noreply.github.com>
2022-02-03 12:15:53 -0800

    Release Build v03

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float mouseSensitivity;
+    [SerializeField] float rollSensitivity;
+    [SerializeField] float playerSpeed;
+    Rigidbody rb;
+    public float maxSpeed = 4000;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        //playerSpeed = 60;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // private void Update()
+    // {
+    //     print(rb.velocity);
+    // }
+
+    void FixedUpdate()
+    {
+        //Any custom axis can be changed in Edit > Project Settings > Input Manager > Axes
+
+        //This will make sure the cursor can always be locked to the screen again, if the player 
+        //has got the cursor in another lockState.
+        if(Cursor.lockState != CursorLockMode.Locked)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+        //This makes the turn the ship left to right, multiplied by sensitivity
+        transform.Rotate(mouseSensitivity * Input.GetAxis("Mouse Y") * Time.deltaTime, mouseSensitivity * Input.GetAxis("Mouse X") * 0.75f * Time.deltaTime, -Input.GetAxis("Roll") * rollSensitivity * Time.deltaTime);
+
+        //This will increase the amount of roll te longer you roll, to give a more dynamic feel
+        if (Input.GetAxis("Roll") != 0)
+        {
+            if (rollSensitivity < 350)
+            {
+                rollSensitivity += 75 / rollSensitivity;
+            }
+        }
+        //In case the player stops rolling, it automatically resets to 100
+        else if(Input.GetAxis("Roll") == 0)
+        {
+            rollSensitivity = 100;
+        }
+
+        //This will make the player move in all directions
+        rb.AddForce(transform.forward * Time.deltaTime * playerSpeed * Input.GetAxis("Vertical"), ForceMode.Impulse);
+        rb.AddForce(transform.up * Time.deltaTime * playerSpeed * 0.9f * Input.GetAxis("Height"), ForceMode.Impulse);
+        rb.AddForce(transform.right * Time.deltaTime * playerSpeed * 0.8f * Input.GetAxis("Horizontal"), ForceMode.Impulse);
+
+        //This will make drag bigger to make it a little less floaty
+        if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0 && Input.GetAxis("Height") == 0)
+        {
+            rb.drag = 2;
+        }
+        else
+        {
+            rb.drag = 0;
+        }
+
+        if (rb.velocity.magnitude > maxSpeed)
+        {
+            rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
+        }
+
+    }
+
+    //This is to make sure the player stays in the body and can accelerate to fun speeds
+    private void OnTriggerEnter(Collider other)
+    {        
+        if(other.tag == "SpeedLimit")
+        {
+            playerSpeed = 500;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "SpeedLimit")
+        {
+            playerSpeed = 300;
+        }
+    }
+}
